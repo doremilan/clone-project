b428fe72e5e591d05cb63ef927d2c9a6fb057cca
dongsun1 <le12352@naver.com>
2022-04-15 15:28:05 +0900

    api 구현 (테스트 미완료) - 동선

--- routers/likes.js
+++ routers/likes.js
@@ -1,4 +1,82 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleWare");
+const Post = require("../schemas/post");
+const Like = require("../schemas/like");
+const User = require("../schemas/user");
+const Unlike = require("../schemas/unlike");
+const Subscribe = require("../schemas/subscribe");
+
+// 좋아요
+router.post("/like", authMiddleware, async (req, res) => {
+  try {
+    const { postNum, likeCheck } = req.body;
+    const { userId } = res.locals.user;
+
+    if (likeCheck) {
+      await Post.updateOne({ postNum }, { $inc: { postLikeNum: -1 } });
+      await Like.deleteOne({ postNum, userId });
+    } else {
+      await Post.updateOne({ postNum }, { $inc: { postLikeNum: 1 } });
+      await Like.create({ postNum, userId });
+    }
+
+    res.status(200).json({ result: true });
+  } catch (error) {
+    console.log(error);
+    console.log("likes.js -> 좋아요에서 에러남");
+
+    res.status(400).json({ result: false });
+  }
+});
+
+// 싫어요
+router.post("/unlike", authMiddleware, async (req, res) => {
+  try {
+    const { postNum, likeCheck } = req.body;
+    const { userId } = res.locals.user;
+
+    if (likeCheck) {
+      await Post.updateOne({ postNum }, { $inc: { postUnlikeNum: -1 } });
+      await Unlike.deleteOne({ postNum, userId });
+    } else {
+      await Post.updateOne({ postNum }, { $inc: { postUnlikeNum: 1 } });
+      await Unlike.create({ postNum, userId });
+    }
+
+    res.status(200).json({ result: true });
+  } catch (error) {
+    console.log(error);
+    console.log("likes.js -> 싫어요에서 에러남");
+
+    res.status(400).json({ result: false });
+  }
+});
+
+// 구독
+router.post("/subscribe", authMiddleware, async (req, res) => {
+  try {
+    const { userSub, subCheck } = req.body;
+    const { userId } = res.locals.user;
+
+    if (subCheck) {
+      await User.updateOne(
+        { userId: userSub },
+        { $inc: { userSubscribe: -1 } }
+      );
+      await Subscribe.deleteOne({ userId: userSub, userId });
+    } else {
+      await User.updateOne({ userId: userSub }, { $inc: { userSubscribe: 1 } });
+      await Subscribe.create({ userId: userSub, userId });
+    }
+
+    res.status(200).json({ result: true });
+  } catch (error) {
+    console.log(error);
+    console.log("likes.js -> 싫어요에서 에러남");
+
+    res.status(400).json({ result: false });
+  }
+});
 
 module.exports = router;
--- routers/search.js
+++ routers/search.js
@@ -1,4 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../schemas/post");
+const User = require("../schemas/user");
+const Subscribe = require("../schemas/user");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const myKey = fs.readFileSync(__dirname + "/../middleware/key.txt").toString();
+
+router.get("/search", async (req, res) => {
+  try {
+    const { keyword } = req.query;
+
+    let posts = await Post.find({ postTitle: { $regex: keyword } }).sort({
+      postDate: -1,
+    });
+
+    for (let user of posts) {
+      let userInfo = await User.findOne({ userId: user.userId });
+      userInfo.userPw = "";
+      user.userInfo = userInfo;
+    }
+
+    const Token = req.headers.authorization;
+    const logInToken = Token.replace("Bearer", "");
+
+    try {
+      const token = jwt.verify(logInToken, myKey);
+      const userId = token.userId;
+      const userSubIds = await Subscribe.findOne({ userId });
+
+      const userSub = [];
+      for (let userSubId of userSubIds) {
+        const subscribeOne = await Subscribe.findOne({
+          userId: userSubId.userId,
+        });
+        if (subscribeOne === null) {
+          continue;
+        } else {
+          userSub.push(subscribeOne);
+        }
+      }
+
+      res.status(200).json({ posts, userSub });
+    } catch (error) {
+      res.status(200).json({ posts });
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("/api/main에서 에러남");
+
+    res.status(404).json({ result: false });
+  }
+});
 
 module.exports = router;
